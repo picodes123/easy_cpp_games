790ce95763ec0c091111bd73c322016c2663f62f
picodes123 <piyush.verma.dev1@gmail.com>
2026-05-17 02:33:20 +0530

    ttt game started

---
+++ tictactoe/ttt.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char board[3][3]{
+        {' ',' ',' '},
+        {' ',' ',' '},
+        {' ',' ',' '}
+    };
+
+    cout<<"-----Welcome to TicTacToe Game-----"<<endl;
+
+    cout << "   |   |   " << endl;
+        cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
+        cout << "___|___|___" << endl;
+        cout << "   |   |   " << endl;
+        cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
+        cout << "___|___|___" << endl;
+        cout << "   |   |   " << endl;
+        cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+        cout << "   |   |   " << endl;
+
+        
+
+
+
+
+
+    return 0;
+}
