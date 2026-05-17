98414a75fb37c01c909faa32989552f3f888c5ad
picodes123 <piyush.verma.dev1@gmail.com>
2026-05-18 02:11:46 +0530

    fixed unput failure and added comments of working

--- tictactoe/ttt.cpp
+++ tictactoe/ttt.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main(){
 
-    const char playerX= 'X';
-    const char playerO= 'O';
-    char currentplayer = playerX;
-    char winner = ' ';
+    const char playerX= 'X';        //player 1
+    const char playerO= 'O';        //player 2
+    char currentplayer = playerX;   //initial move player X
+    char winner = ' ';              //initialize winner with empty space
 
 
     int c=-1;
     int r=-1;
 
-    char board[3][3]{
+    char board[3][3]{   //initialize array for board
         {' ',' ',' '},
         {' ',' ',' '},
         {' ',' ',' '}
@@ -26,7 +26,7 @@ int main(){
      
 
         cout << "   |   |   " << endl;
-        cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
+        cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;   //grid of board
         cout << "___|___|___" << endl;
         cout << "   |   |   " << endl;
         cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
@@ -36,41 +36,49 @@ int main(){
         cout << "   |   |   " << endl; 
 
         
-        if (winner!=' ')
+        if (winner!=' ')  //break if winner found
         {
             break;
         }
 
-        cout<<"now the move is of: "<<currentplayer<<endl;
+        cout<<"now the move is of: "<<currentplayer<<endl; //tells whos move
 
-        while (true)
+        while (true) //used to take valid input
         {
         cout<<"put value of r c from 0-2 for row and column"<<endl;
         cin>>r>>c;
+
+        if (cin.fail()) //check input failure
+        {
+             cin.clear();
+             cin.ignore(10000,'\n');
+             cout<<"Choose valid input"<<endl;
+        }
         
-         if (r<0||r>2||c<0||c>2)
+        else if (r<0||r>2||c<0||c>2) //failure when btw 0-2
         {
             cout<<"chose between 0-2"<<endl;
         }
-        else if (board[r][c]!=' ')
+
+        else if (board[r][c]!=' ') //check when tile already have value
         {
             cout<<"tile is full try again"<<endl;
         }
+
         else
         {
             break;
         }}
 
 
-         board[r][c]=currentplayer;
-        currentplayer= (currentplayer==playerX) ? playerO : playerX; 
+        board[r][c]=currentplayer; //put XorO on board
+        currentplayer= (currentplayer==playerX) ? playerO : playerX; //change player
 
 
-        cin.clear();
-        cin.ignore(10000,'\n');
+       
 
 
-        for (int r = 0; r < 3; r++)
+        for (int r = 0; r < 3; r++)  //win condition for rows
         {
             if (board[r][0]==board[r][1]&&board[r][1]==board[r][2]&&board[r][0]!=' ')
             {
@@ -80,7 +88,7 @@ int main(){
             
         }
 
-        for (int c = 0; c < 3; c++)
+        for (int c = 0; c < 3; c++) //win condition for columns 
         {
             if (board[0][c]==board[1][c]&&board[1][c]==board[2][c]&&board[0][c]!=' ')
             {
@@ -90,12 +98,12 @@ int main(){
             
         }
         
-        if (board[0][0]==board[1][1]&&board[1][1]==board[2][2]&&board[0][0]!=' ')
+        if (board[0][0]==board[1][1]&&board[1][1]==board[2][2]&&board[0][0]!=' ')  //win condi for left diago
         {
             winner=board[0][0];
             
         }
-        else if (board[0][2]==board[1][1]&&board[1][1]==board[2][0]&&board[0][2]!=' ')
+        else if (board[0][2]==board[1][1]&&board[1][1]==board[2][0]&&board[0][2]!=' ')  //win condi for right diago
         {
             winner=board[0][2];
             
@@ -105,7 +113,7 @@ int main(){
     }
 
 
-     if (winner!=' ')
+     if (winner!=' ') //tells the winner or tells if its a tie 
         {
             cout<<"the winner is: "<<winner<<endl;
         }
@@ -115,12 +123,4 @@ int main(){
             cout<<"the game is tie!!"<<endl;
         }
         
-
-        
-
-
-
-
-
-    
 }
